e90c6a4a6b1a048896694cacc4c035c0457254d0
NIBIN G VARGHESE <mailtonibin2011@gmail.com>
2021-06-12 23:28:08 +0530

    first commit

---
+++ src/test/java/com/nibin/codes/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nibin.codes.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
